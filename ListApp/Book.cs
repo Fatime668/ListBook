58a8c2e9536bd812ee94dc1aa16a9e308cb3687c
Fatime668 <tu7dkjdue@code.edu.az>
2022-05-01 08:04:16 +0400

    List

---
+++ ListApp/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListApp
+{
+    class Book
+    {
+        private static int _id=10;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+        public string Code { get; set; }
+        public Book(string name,string authorname,int pagecount,string code)
+        {
+            _id++;
+            Id = _id;
+            Name = name;
+            AuthorName = authorname;
+            PageCount = pagecount;
+            Code = code.Substring(0, 2).ToUpper() + Id;
+        }
+    }
+}
---
+++ ListApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Kitab-1","kitabca",100,"programming");
+            Book book1 = new Book("Kitab-2","kitab",200,"backend");
+            Library lib = new Library();
+            List<Book> bk = lib.FindAllBookByName(book.Name);
+            bk.Add(book);
+            bk.Add(book1);
+            foreach (var item in bk)
+            {
+                Console.WriteLine(item.Code);
+            }
+            //foreach (var item in bk)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+            Console.WriteLine(book.Code);
+            List<Book> books = new List<Book> { book, book1 };
+            foreach (var item in books)
+            {
+                Console.WriteLine("Name: " + item.Name + " " + "AuthorName: " + item.AuthorName + " " + "Code: " + item.Code + " " + "PageCount: " + item.PageCount);
+
+            }
+        }
+    }
+}
